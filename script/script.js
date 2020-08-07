9baa08f72bd223b1ecce5a6a11ba5bcb38c21a6f
Katya <13gordienkok@gmail.com>
2020-08-07 13:28:02 +0300

    First commit

--- script/script.js
+++ script/script.js
@@ -1,9 +1,9 @@
 alert('Hello frends');
 let money = 5;
-let income;
-let addExpenses;
-const deposit;
-let mission;
-led period = 10;
+let income = 'string';
+let addExpenses = 'true';
+let deposit = 156;
+let mission = 'Hemo';
+let period;
 console.log(money);
-alert(period);
+alert(deposit);
